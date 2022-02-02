22ae4839e84f8ada84ef8cae4f38192940f48fc9
Andy Rodríguez <androdl96@gmail.com>
2022-02-02 17:04:05 -0500

    lcd added in e3

--- e3_distance_LCD/e3_distance_LCD.ino
+++ e3_distance_LCD/e3_distance_LCD.ino
@@ -1,6 +1,9 @@
+#include <LiquidCrystal.h>
+
 #define trigPin 13
 #define echoPin 12
 int duration,distance;
+LiquidCrystal lcd(6,7,8,9,10,11);
 
 void measure();
 
@@ -8,7 +11,9 @@ void setup() {
   pinMode(trigPin, OUTPUT);
   digitalWrite(trigPin,LOW);
   attachInterrupt(0, measure, RISING);
-  Serial.begin(9600);
+  //Serial.begin(9600);
+  lcd.begin(16,2);
+  lcd.print("Distance :");
 }
 
 void loop() {
@@ -20,6 +25,12 @@ void measure(){
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
   distance = ((duration/2) / 29.1);
-  Serial.print(distance);
-  Serial.println(" cm");
+  //Serial.print(distance);
+  //Serial.println(" cm");
+
+  lcd.setCursor(0,1);
+  lcd.print("        ");
+  lcd.setCursor(2,1);
+  lcd.print(distance);
+  lcd.print(" cm");
   }
